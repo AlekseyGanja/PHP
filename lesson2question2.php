3fc3a8f9ed84fb90b44830a6734eccc5cfa46936
Aleksey Ganja <aleksey-ganja@mtu-net.ru>
2020-05-20 21:23:17 +0300

    Первая  часть ДЗ2

---
+++ lesson2question2.php
@@ -0,0 +1,57 @@
+<?php
+$title="<TITLE>Homework2question2</TITLE>";
+echo $title;
+$a=rand(0,15);
+echo"\$a=$a ";
+?>
+<br><br>
+при помощи switch case
+<br>
+<? 
+  switch ($a){
+	case 0:
+		echo "0 ";
+	case 1:
+		echo "1 ";
+	case 2:
+		echo "2 ";
+	case 3:
+		echo "3 ";
+	case 4:
+		echo "4 ";
+	case 5:
+		echo "5 ";
+	case 6:
+		echo "6 ";
+	case 7:
+		echo "7 ";
+	case 8:
+		echo "8 ";
+	case 9:
+		echo "9 ";
+	case 10:
+		echo "10 ";
+	case 11:
+		echo "11 ";
+	case 12:
+		echo "12 ";
+	case 13:
+		echo "13 ";
+	case 14:
+		echo "14 ";
+	case 15:
+		echo "15 ";
+}
+?>
+<br><br>
+при помощи рекурсии
+<br>
+<?php
+function outFifteen($a){
+	if ($a<=15){
+		echo $a++." ";
+		outFifteen($a);
+	};
+};
+outFifteen($a);
+?>
