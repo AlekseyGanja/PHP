d43489167ee1c12190321ba7ebe88c957cccfda7
Aleksey Ganja <aleksey-ganja@mtu-net.ru>
2020-05-31 20:33:46 +0300

    Добавил задачу с указанием просмотров полноразмерного фото

--- lesson5question2.php
+++ lesson5question2.php
@@ -4,7 +4,7 @@ $files = scandir("pictures_big");
 include "lesson5question2config.php";//подключаемся к БД
 for($i=2;$i<count($files);$i++):
     $name=$files[$i];
-    $path="php/pictures_big";
+    $path="pictures_big";
     $size=filesize('pictures_big/'.$files[$i]);
     $sql="INSERT INTO `pictures` (`name`, `path`, `size`) VALUES ('".$name."','".$path."',".$size.")";//здесь был бой с кавычками
     if (mysqli_query($connect, $sql)){
